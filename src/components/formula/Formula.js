c86bff97071102d0fd76b99dea0d60edc5eee42d
Ihor Tkachuk <I.Tka4yk@gmail.com>
2020-05-17 18:22:04 +0300

    feat: Finish base framework functional

---
+++ src/components/formula/Formula.js
@@ -0,0 +1,30 @@
+import {ExcelComponent} from '@core/ExcelComponent';
+
+export class Formula extends ExcelComponent {
+  static className = 'excel__formula';
+
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input'],
+    });
+  }
+
+  toHTML() {
+    return `
+      <div class="info">fx</div>
+      <div class="input" contenteditable spellcheck="false"></div>
+    `;
+  }
+
+  onInput() {
+    console.log('Formula: onInput', event.target.textContent.trim());
+    console.log(this.$root);
+
+    if (event.target.textContent.trim() === 'quit') {
+      /* eslint-disable no-debugger */
+      // debugger;
+      this.removeDOMListeners();
+    }
+  }
+}
